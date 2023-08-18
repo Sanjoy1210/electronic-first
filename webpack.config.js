ab9486ac72b5d50114131429b5483f851c15ae3c
Sanjoy1210 <sanjoypp@gmail.com>
2023-08-18 17:45:59 +0600

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,57 @@
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const webpack = require("webpack");
+
+// module.exports = ({ mode }) => {
+//   return {
+//     mode,
+//     resolve: {
+//       extensions: [".js"],
+//     },
+//     entry: "./src/index.js",
+//     output: {
+//       filename: "dist.js",
+//       assetModuleFilename: "[name].[ext]",
+//     },
+//     module: {
+//       rules: [
+//         {
+//           test: /\.css$/,
+//           use: [MiniCssExtractPlugin.loader, "css-loader"],
+//         },
+//         {
+//           test: /\.scss$/,
+//           use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+//         },
+//         {
+//           test: /\.(png|jpe?g|gif|svg)$/i,
+//           use: [
+//             {
+//               loader: "file-loader",
+//               options: {
+//                 name: "[name].[ext]",
+//                 outputPath: "images", // Output directory for images
+//               },
+//             },
+//           ],
+//         },
+//       ],
+//     },
+//     plugins: [
+//       new webpack.ProgressPlugin(),
+//       new MiniCssExtractPlugin(),
+//       new HtmlWebpackPlugin({
+//         template: "src/index.html",
+//       }),
+//     ],
+//   };
+// };
+
+
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
+const path = require("path");
 
 module.exports = ({ mode }) => {
   return {
@@ -11,7 +62,7 @@ module.exports = ({ mode }) => {
     entry: "./src/index.js",
     output: {
       filename: "dist.js",
-      assetModuleFilename: "images",
+      path: path.resolve(__dirname, "dist"),
     },
     module: {
       rules: [
@@ -25,15 +76,10 @@ module.exports = ({ mode }) => {
         },
         {
           test: /\.(png|jpe?g|gif|svg)$/i,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                name: "[name].[ext]",
-                outputPath: "images", // Output directory for images
-              },
-            },
-          ],
+          type: "asset/resource",
+          generator: {
+            filename: "images/[name][ext]",
+          },
         },
       ],
     },
